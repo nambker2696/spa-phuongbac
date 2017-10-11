616f0072a125d97143f1cdd1b7c7ce9883c724e2
NamBKER <nguyenvannam20132696@gmail.com>
2017-10-11 15:11:56 +0700

    Mutil language

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -47,6 +47,20 @@
                             </li>
                             <li><a href="about.html" >Địa chỉ</a></li>
                             <li><a href="contact.html" >Liên lạc</a></li>
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                    {{ Config::get('languages')[App::getLocale()] }}
+                                </a>
+                                <ul class="dropdown-menu">
+                                    @foreach (Config::get('languages') as $lang => $language)
+                                        @if ($lang != App::getLocale())
+                                            <li>
+                                                <a href="{{ route('lang.switch', $lang) }}">{{$language}}</a>
+                                            </li>
+                                        @endif
+                                    @endforeach
+                                </ul>
+                            </li>
                         </ul>
                     </nav>
                 </div>
