9bfde93f7d0c359cb4b54409ad386ebc0878d52e
NamBKER <nguyenvannam20132696@gmail.com>
2017-10-11 11:39:10 +0700

    admin

--- config/app.php
+++ config/app.php
@@ -79,6 +79,7 @@ return [
     */
 
     'locale' => 'en',
+    'locales' => ['en' => 'English', 'sv' => 'Swedish'],
 
     /*
     |--------------------------------------------------------------------------
@@ -225,7 +226,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        'MultiLang' => Longman\LaravelMultiLang\Facades\MultiLang::class,
     ],
 
 ];
